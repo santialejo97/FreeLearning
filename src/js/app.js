1ed1634cefa94e730457688d8e8fb5fb2b7d2e55
santialejo97 <gaviriasantiago1997@gmail.com>
2021-02-26 16:55:11 -0500

    diseño de formulario terminado

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    iniciarApp();   
+})
+
+function inicarApp(){
+//   conexion de Api
+  conectarApi();
+}
+
+async function conectarApi(){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
