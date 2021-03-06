f40d51fb12964a5bf6543cfd5f9ddc7a04de83c5
santialejo97 <gaviriasantiago1997@gmail.com>
2021-03-06 17:43:09 -0500

    creacion de formulario de empleados

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
+// paginado de los formularios
+let pagina=1;
 
+// objeto que se envia a la base de datos 
 const usuario={
   usuarioNombre: '',
   usuarioEmail: '',
@@ -16,13 +19,19 @@ function iniciarPagina(){
   //LLenar selector de las carrera de la Universidad
   llenarSelector();
 
-  // Capturamos la informqacion de formulario 
-  capturaDatos();
-
   // enviar objeto 
   enviarObjeto();
 
+  // asigna la clase de mostrarseccion actual en la pagina 
+  mostrarSeccion();
 
+  // cambiamos la seccion al formulario elegido
+  cambiarSeccion();
+
+  // Capturamos la informacion de formulario estudiantes
+  capturaDatosEstudiante();
+
+  capturaDatosEmpleado();
 }
 
 // se trae infromacion de la base dedatos y se llena el selector 
@@ -32,14 +41,13 @@ async function llenarSelector(){
   try {
     const carrera= await fetch(urlcarrera,{method:'GET'})
     const data = await carrera.json();
-    console.log(data)
-     // se selecciona el select  
-  const selector= document.querySelector('#carrera')
+    
+  // se selecciona el select  
+    const selector= document.querySelector('#carrera')
 
   // se recorrer el arreglo de las carreras
   data.forEach(carrera =>{
     const {carreraNombre, carreraId}= carrera;
-    console.log(carreraNombre)
 
   //se crear elemneto option para agregar al select y se le asigna una carrera de el arreglo
     const option = document.createElement('OPTION');
@@ -60,47 +68,73 @@ async function llenarSelector(){
  function conectarApi(){
   //  se conecta a el BackEnd y se realiza un metodo POST
     try {
-      const url = 'http://localhost:3000/api/estudiantes/'
-      console.log(usuario)
-      fetch(url,{method:'POST',
-      body: JSON.stringify({
-        estudianteNombre:  usuario.usuarioNombre,
-        estudianteEmail:usuario.usuarioEmail,
-        estudiantePassword: usuario.usuarioPassword,
-        estudiantePoliticaDatos: usuario.usuarioPoliticaDatos,
-        fk_carreraId: usuario.usuarioCarrera
-      }),
-      headers: {
-        'Content-Type': 'application/json'
+      let tabla;
+      let url ;
+      if(pagina === 1){
+        tabla='estudiantes'
+        url=`http://localhost:3000/api/${tabla}/`
+        console.log(usuario)
+        fetch(url,{method:'POST',
+        body: JSON.stringify({
+          estudianteNombre:usuario.usuarioNombre,
+          estudianteEmail:usuario.usuarioEmail,
+          estudiantePassword: usuario.usuarioPassword,
+          estudiantePoliticaDatos: usuario.usuarioPoliticaDatos,
+          fk_carreraId: usuario.usuarioCarrera
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)})
+      }else if(pagina === 2){
+        tabla='empleados'
+        url=`http://localhost:3000/api/${tabla}/`
+        console.log(usuario)
+        fetch(url,{method:'POST',
+        body: JSON.stringify({
+          empleadoNombre:usuario.usuarioNombre,
+          empleadoEmail:usuario.usuarioEmail,
+          empleadoPassword: usuario.usuarioPassword,
+          empleadoPoliticaDatos: usuario.usuarioPoliticaDatos,
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)})
       }
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)})
+     
     } catch (error) {
         console.log(error)
     }
 }
 
 // funcion de captura de datos 
-function capturaDatos(){
+function capturaDatosEstudiante(){
   // Capturamos el dato de nombre 
   const nombreInput= document.querySelector('#nombre')
   nombreInput.addEventListener('input', (e)=>{
     usuario.usuarioNombre= e.target.value.trim();
-    
+    console.log(usuario) 
   })
   
   // Capturamos el dato de Carrera Universidad
   const carreraSelector=document.querySelector('#carrera');
   carreraSelector.addEventListener('click', e=>{
   usuario.usuarioCarrera= parseInt(e.target.value);
+  console.log(usuario)
   })
 
   // Capturamos el correo del usuario
   const correoInput=document.querySelector('#email');
   correoInput.addEventListener('input',(e)=> {
     usuario.usuarioEmail=e.target.value.trim();
+    console.log(usuario)
     
   })
 
@@ -108,9 +142,26 @@ function capturaDatos(){
   const passInput= document.querySelector('#password');
   passInput.addEventListener('input', (e)=>{
     usuario.usuarioPassword=e.target.value.trim();
+    console.log(usuario)
   }) 
-  
- 
+
+}
+
+function capturaDatosEmpleado(){
+ const nombreEmpleado = document.querySelector('#nombreempleado');
+ nombreEmpleado.addEventListener('input', e =>{
+   usuario.usuarioNombre = e.target.value.trim();
+ })
+
+ const correoEmpleado= document.querySelector('#emailempleado');
+ correoEmpleado.addEventListener('input', e =>{
+   usuario.usuarioEmail= e.target.value.trim();
+ })
+
+ const contraseñaEmpleado= document.querySelector('#passwordempleado');
+ contraseñaEmpleado.addEventListener('input', e =>{
+   usuario.usuarioPassword= e.target.value.trim();
+ })
 }
 
 // funcion de las alarmas
@@ -123,9 +174,14 @@ function mostrarAlerta(mensaje , tipo){
   }else{
     alerta.classList.add('creado');
   }
-  const formulario= document.querySelector('.formulario_usuarios');
-  formulario.appendChild(alerta);
 
+  if(pagina=== 1){
+    const formulario= document.querySelector('.mensaje1');
+  formulario.appendChild(alerta);
+  }else{
+    const formulario2= document.querySelector('.mensaje2');
+  formulario2.appendChild(alerta);
+  }
   setTimeout(()=>{
     alerta.remove();
   }, 5000)
@@ -145,26 +201,51 @@ function politicasAceptacion(){
 
 // validacion de campos obligatorios 
 function validacionCampos(){
+  if(pagina === 1){
   if(usuario.usuarioNombre === '' || usuario.usuarioCarrera === '' || usuario.usuarioEmail === '' 
-  || usuarioPassword === ''){
+  || usuario.usuarioPassword === ''){
    mostrarAlerta("Es necesario llenar nombre, contraseña, carrera y correo ", 'error');
    console.log(usuario)
-   e.preventDefault;
   }else{
    mostrarAlerta("Usuario Creado Correctamente", 'correcto');
    conectarApi();
    console.log('estoy enviando ....')
    console.log(usuario)
   }
+  }else{
+    if(usuario.usuarioNombre === '' || usuario.usuarioEmail === '' 
+  || usuario.usuarioPassword === ''){
+   mostrarAlerta("Es necesario llenar nombre, contraseña y correo ", 'error');
+   console.log(usuario)
+   console.log(pagina)
+  }else{
+   mostrarAlerta("Usuario Creado Correctamente", 'correcto');
+   conectarApi();
+   console.log(pagina)
+   console.log('estoy enviando ....')
+   console.log(usuario)
+  }
+  } 
 }
 
 // de ejecucion de el envio del usuario 
 function enviarObjeto(){
   const boton = document.querySelector('#envio')
   boton.addEventListener('click',(e)=>{
+// se valida la informacion que se captura 
+if(pagina === 1){
+  console.log(pagina)
+   // Capturamos la informacion de formulario estudiantes
+   capturaDatosEstudiante();
+}else{
+  console.log(pagina)
+  // Capturamos la informacion de formulario empleado
+  capturaDatosEmpleado();
+}
+    console.log(usuario)
 // validamos la aceptacion de las politicas 
-politicasAceptacion();
- //   validacion de los campos y la ejecucion con el API
+ politicasAceptacion();
+ //validacion de los campos y la ejecucion con el API
  validacionCampos();
 })
 }
