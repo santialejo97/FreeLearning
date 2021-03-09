f7e9e84cf403c652be1c6ba35b80ee0373a1a40c
Marlon-Camacho <marloncamacho6@gmail.com>
2021-03-08 21:37:42 -0500

    cambios formulario actualizacion

--- src/js/actualiza/actualizar.js
+++ src/js/actualiza/actualizar.js
@@ -1,15 +1,22 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    iniciarPaginaAct();   
+});
+
+function iniciarPaginaAct(){
+    traerDatos();
+};
 
 async function traerDatos(){
     try{
-        const urlestudiante = 'http://localhost:3000/api/estudiantes';
+        const urlestudiante = 'http://localhost:3000/api/estudiantes/';
         const estudianteact = await fetch(urlestudiante, {method:'GET'})
         const data = await estudianteact.json();    
         console.log(data)
         data.forEach(element => {
-            const {estudianteEmail, estudianteNombre, estudiantePassword}=element;
-            const nombre= document.querySelector('#nombreActualizacion');
-            console.log(estudianteNombre)
-            
+            const {estudianteNombre}=element;
+            const nombre= document.querySelector('#nombreActualizacion');  
+            nombre.value=estudianteNombre;
+              
         });
     }catch (error) {
         console.log(error)
